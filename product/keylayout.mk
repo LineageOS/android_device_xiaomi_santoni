3c8b72ed2469356341ace1d6a2e2a720fb52008c
Isaac Chen <isaacchen@isaacchen.cn>
2018-04-06 14:24:36 +0200

    santoni: Move configs to vendor
    Signed-off-by: Isaac Chen <isaacchen@isaacchen.cn>

--- product/keylayout.mk
+++ product/keylayout.mk
@@ -1,3 +1,3 @@
 # Keylayouts
 PRODUCT_COPY_FILES += \
-    $(DEVICE_PATH)/keylayout/msm8940-sku6-snd-card_Button_Jack.kl:system/usr/keylayout/msm8940-sku6-snd-card_Button_Jack.kl
+    $(DEVICE_PATH)/keylayout/msm8940-sku6-snd-card_Button_Jack.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/msm8940-sku6-snd-card_Button_Jack.kl
