a23bcbebd325712060d72e33043728ef5ab64c7d
Isaac Chen <isaacchen@isaacchen.cn>
2018-07-08 13:13:13 +0200

    santoni: Set vendor security patch level

--- lineage.mk
+++ lineage.mk
@@ -42,3 +42,6 @@ BUILD_FINGERPRINT := Xiaomi/santoni/santoni:7.1.2/N2G47H/V9.2.1.0.NAMCNEK:user/r
 
 PRODUCT_SYSTEM_PROPERTY_BLACKLIST += \
     ro.product.model
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.build.security_patch=2018-06-01
